23d5f36023fd77feb33ac1bb113b9d671a629496
ngem1 <77919455+ngem1@users.noreply.github.com>
2025-01-21 20:41:49 +0000

    Update cover.py

--- bus_t4/cover.py
+++ bus_t4/cover.py
@@ -1,32 +1,36 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import cover
 from esphome.const import CONF_ADDRESS, CONF_ID, CONF_UPDATE_INTERVAL, CONF_USE_ADDRESS
 
-# Avoid importing the whole cover module directly
-from esphome.components import cover as cover_ns
+
 
 bus_t4_ns = cg.esphome_ns.namespace('bus_t4')
-Nice = bus_t4_ns.class_('NiceBusT4', cover_ns.Cover, cg.Component)
+Nice = bus_t4_ns.class_('NiceBusT4', cover.Cover, cg.Component)
 
-# Debugging prints
-print("Debug: Namespace and NiceBusT4 class created successfully")
-
-CONFIG_SCHEMA = cv.Schema({
+CONFIG_SCHEMA = cover.COVER_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(Nice),
     cv.Optional(CONF_ADDRESS): cv.hex_uint16_t,
     cv.Optional(CONF_USE_ADDRESS): cv.hex_uint16_t,
-}).extend(cv.COMPONENT_SCHEMA).extend(cover_ns.COVER_SCHEMA)
+#    cv.Optional(CONF_UPDATE_INTERVAL): cv.positive_time_period_milliseconds,
+}).extend(cv.COMPONENT_SCHEMA)
+
 
 def to_code(config):
-    print("Debug: Running to_code function")
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
-    yield cover_ns.register_cover(var, config)
+
+    yield cover.register_cover(var, config)
 
     if CONF_ADDRESS in config:
-        cg.add(var.set_to_address(config[CONF_ADDRESS]))
-        print(f"Debug: Address set to {config[CONF_ADDRESS]}")
+        address = config[CONF_ADDRESS]
+        cg.add(var.set_to_address(address))
 
     if CONF_USE_ADDRESS in config:
-        cg.add(var.set_from_address(config[CONF_USE_ADDRESS]))
-        print(f"Debug: Use address set to {config[CONF_USE_ADDRESS]}")
+        use_address = config[CONF_USE_ADDRESS]
+        cg.add(var.set_from_address(use_address))
+        
+        
+ #   if CONF_UPDATE_INTERVAL in config:
+ #       update_interval = config[CONF_UPDATE_INTERVAL]
+ #       cg.add(var.set_update_interval(update_interval))
